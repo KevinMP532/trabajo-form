e210095ec6b01684fc9948b68835c7bc7c57bcec
Kevin Mora Pais <kevinmorapais532@gmail.com>
2021-05-12 23:35:40 -0300

    A

--- Frontened/script.js
+++ Frontened/script.js
@@ -4,11 +4,7 @@ function myFunction() {
     method: 'POST',
     url: 'http://localhost/trabajo-form/Backend/endpoints/visualizarPeliculas.php'
   }).then(response => {
-
-
-
-
-    console.log(response)
+    console.log(response.data)
 
   }).catch(function (error) {
     console.log(error);
