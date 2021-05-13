345d5106309b189f3f385cbd52cd2a23b03c53b0
brayan marcano <brayan.marcano@xmartlabs.com>
2021-05-13 20:54:32 -0300

    pantalla

--- Frontened/script.js
+++ Frontened/script.js
@@ -1,4 +1,25 @@
 
+function sexo() {
+  axios({
+    method: 'POST',
+    url: 'http://localhost/trabajo-form/Backend/endpoints/visualizarPeliculas.php'
+  }).then(response => {
+    for(let i=0;i < response.data.length;i++ ){
+      let lulu = document.getElementById("imgbox")
+      lulu.innerHTML = response.data[i].img
+      var html = ""
+      html+='<div>'+response.data[i].nombre+'</div>';
+      document.getElementById('imgbox').innerHTML+= html
+      return(response.data[i])
+    }
+
+
+  }).catch(function (error) {
+    console.log(error);
+  })
+}
+
+
 function myFunction() {
   axios({
     method: 'POST',
